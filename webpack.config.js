3bacf8d1cb0dd71833317931a4e5dc3b51154b9c
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-11-18 17:38:56 +0900

    add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { loader } = require('mini-css-extract-plugin');
-// production モード以外の場合、変数 enabledSourceMap は true
+// production モード以外の場合、変数 enabledSourceMap は true(本番では重くなるので要注意)
 const enabledSourceMap = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+  mode: 'development', //デフォルトはproduction
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, './dist'), //絶対パス。第一引数(__dirname)で現在のフォルダ階層を示す
@@ -61,7 +62,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(jpg|png)$/,
+        test: /\.(jpg|jpeg|png)$/, //画像は'image-webpack-loader'で圧縮後、webpack5/Asset Modulesで正しく読み込まれる
         //webpack5ではfile-loaderやurl-loaderを使わないでも'type:','generator:'の記述で画像が正しく表現できる
         type: 'asset/resource',
         generator: {
@@ -77,6 +78,16 @@ module.exports = {
           //     name: 'images/[name].[ext]', //ランダムな文字列にならないよう、nameを指定する
           //   },
           // },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              //その他画像指定については https://github.com/tcoopman/image-webpack-loader
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
         ],
       },
       {
